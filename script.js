4d1895e7bb05241453d84bddf09def787d463e7b
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-10-26 15:14:25 -0500

    background color fun

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 let header = document.querySelector('header');
 
 header.addEventListener('click', () => {
-	alert('You clicked on the header');
+	window.location.href = './index.html';
 });
+
+const colors = [
+	'rgba(255, 191, 0, .25)',
+	'rgba(0, 128, 255, .25)',
+	'rgba(1, 223, 58, .25)',
+	'rgba(255, 0, 128, .25)'
+];
+
+randomColor = array => {
+	backgroundColor = array[Math.floor(Math.random() * array.length)];
+	return backgroundColor;
+};
+
+changeBackgroundColor = () => {
+	var homeDivs = document.getElementsByClassName('home');
+	for (let i = 0; i < homeDivs.length; i++) {
+		homeDivs[i].style.backgroundColor = randomColor(colors);
+		homeDivs[i].addEventListener('mouseenter', function(event) {
+			console.log(event.target);
+			event.target.style.backgroundColor = randomColor(colors);
+		});
+	}
+};
+
+changeBackgroundColor();
