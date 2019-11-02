7905e3e18ff440707071956a7e0c148f6c763b60
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-02 15:42:11 -0500

    home page redesign

--- script.js
+++ script.js
@@ -4,27 +4,24 @@ header.addEventListener('click', () => {
 	window.location.href = './index.html';
 });
 
-const colors = [
-	'rgba(255, 191, 0, .25)',
-	'rgba(0, 128, 255, .25)',
-	'rgba(1, 223, 58, .25)',
-	'rgba(255, 0, 128, .25)'
-];
+//save for later:
+// const colors = [
+// 	'rgba(255, 191, 0, .1)',
+// 	'rgba(0, 128, 255, .1)',
+// 	'rgba(1, 223, 58, .1)',
+// 	'rgba(255, 0, 128, .1)'
+// ];
 
-randomColor = array => {
-	backgroundColor = array[Math.floor(Math.random() * array.length)];
-	return backgroundColor;
-};
+// randomColor = array => {
+// 	backgroundColor = array[Math.floor(Math.random() * array.length)];
+// 	return backgroundColor;
+// };
 
-changeBackgroundColor = () => {
-	var homeDivs = document.getElementsByClassName('home');
-	for (let i = 0; i < homeDivs.length; i++) {
-		homeDivs[i].style.backgroundColor = randomColor(colors);
-		homeDivs[i].addEventListener('mouseenter', function(event) {
-			console.log(event.target);
-			event.target.style.backgroundColor = randomColor(colors);
-		});
-	}
-};
-
-changeBackgroundColor();
+// changeBackgroundColor = () => {
+// 	const homeDivs = document.getElementsByClassName('home');
+// 	let color = randomColor(colors);
+// 	for (let i = 0; i < homeDivs.length; i++) {
+// 		homeDivs[i].style.backgroundColor = color;
+// 	}
+// };
+// changeBackgroundColor();
