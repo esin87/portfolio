20e53c35dfafdc5665b635dcba82865cd79b263d
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-02 20:21:15 -0500

    finalized for now

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
-let header = document.querySelector('header');
+//profile picture switcheroo
+let esin = document.getElementById('esin-picture');
 
-header.addEventListener('click', () => {
-	window.location.href = './index.html';
+esin.addEventListener('mouseover', () => {
+	if (esin.getAttribute('src') == 'images/esinbw2.jpeg') {
+		esin.setAttribute('src', 'images/esin_bookshelf.jpg');
+	} else {
+		esin.setAttribute('src', 'images/esinbw2.jpeg');
+	}
 });
 
-//save for later:
-// const colors = [
-// 	'rgba(255, 191, 0, .1)',
-// 	'rgba(0, 128, 255, .1)',
-// 	'rgba(1, 223, 58, .1)',
-// 	'rgba(255, 0, 128, .1)'
-// ];
+//social icon hijinks
+let linkedin = document.getElementById('linkedin');
+let github = document.getElementById('github');
+let instagram = document.getElementById('instagram');
+let email = document.getElementById('email');
+let resume = document.getElementById('resume');
 
-// randomColor = array => {
-// 	backgroundColor = array[Math.floor(Math.random() * array.length)];
-// 	return backgroundColor;
-// };
+linkedin.addEventListener('mouseover', () => {
+	if (linkedin.getAttribute('src') == 'images/icons/045-linkedin.png') {
+		linkedin.setAttribute('src', 'images/icons/001-linkedin.png');
+	} else {
+		linkedin.setAttribute('src', 'images/icons/045-linkedin.png');
+	}
+});
 
-// changeBackgroundColor = () => {
-// 	const homeDivs = document.getElementsByClassName('home');
-// 	let color = randomColor(colors);
-// 	for (let i = 0; i < homeDivs.length; i++) {
-// 		homeDivs[i].style.backgroundColor = color;
-// 	}
-// };
-// changeBackgroundColor();
+github.addEventListener('mouseover', () => {
+	if (github.getAttribute('src') == 'images/icons/038-github.png') {
+		github.setAttribute('src', 'images/icons/github.png');
+	} else {
+		github.setAttribute('src', 'images/icons/038-github.png');
+	}
+});
+
+instagram.addEventListener('mouseover', () => {
+	if (instagram.getAttribute('src') == 'images/icons/025-instagram.png') {
+		instagram.setAttribute('src', 'images/icons/002-instagram.png');
+	} else {
+		instagram.setAttribute('src', 'images/icons/025-instagram.png');
+	}
+});
+
+email.addEventListener('mouseover', () => {
+	if (
+		email.getAttribute('src') ==
+		'images/icons/black-back-closed-envelope-shape.png'
+	) {
+		email.setAttribute('src', 'images/icons/003-gmail.png');
+	} else {
+		email.setAttribute(
+			'src',
+			'images/icons/black-back-closed-envelope-shape.png'
+		);
+	}
+});
+
+resume.addEventListener('mouseover', () => {
+	if (resume.getAttribute('src') == 'images/icons/005-profile-1.png') {
+		resume.setAttribute('src', 'images/icons/004-profile.png');
+	} else {
+		resume.setAttribute('src', 'images/icons/005-profile-1.png');
+	}
+});
