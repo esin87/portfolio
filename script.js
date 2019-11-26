d705771dcada00f5a52b58495c5414046c7e2e65
Esin Saribudak <esin.saribudak@gmail.com>
2019-11-26 15:54:03 -0600

    icon bar changes and external image hosting for faster load

--- script.js
+++ script.js
@@ -1,62 +1,33 @@
 //social icon hijinks
+let iconBox = document.getElementById('icon-container');
+
 let linkedin = document.getElementById('linkedin');
 let github = document.getElementById('github');
 let instagram = document.getElementById('instagram');
 let email = document.getElementById('email');
 let resume = document.getElementById('resume');
 
-linkedin.addEventListener('mouseover', () => {
-	if (linkedin.getAttribute('src') == 'images/icons/045-linkedin.png') {
-		linkedin.setAttribute('src', 'images/icons/001-linkedin.png');
-	}
-	// } else {
-	// 	linkedin.setAttribute('src', 'images/icons/045-linkedin.png');
-	// }
+iconBox.addEventListener('mouseover', () => {
+	linkedin.setAttribute('src', 'images/icons/001-linkedin.png');
+	github.setAttribute('src', 'images/icons/github.png');
+	instagram.setAttribute('src', 'images/icons/002-instagram.png');
+	email.setAttribute('src', 'images/icons/003-gmail.png');
+	resume.setAttribute('src', 'images/icons/004-profile.png');
 });
 
-github.addEventListener('mouseover', () => {
-	if (github.getAttribute('src') == 'images/icons/038-github.png') {
-		github.setAttribute('src', 'images/icons/github.png');
-	}
-	// } else {
-	// 	github.setAttribute('src', 'images/icons/038-github.png');
-	// }
-});
-
-instagram.addEventListener('mouseover', () => {
-	if (instagram.getAttribute('src') == 'images/icons/025-instagram.png') {
-		instagram.setAttribute('src', 'images/icons/002-instagram.png');
-	}
-	// } else {
-	// 	instagram.setAttribute('src', 'images/icons/025-instagram.png');
-	// }
-});
-
-email.addEventListener('mouseover', () => {
-	if (
-		email.getAttribute('src') ==
+iconBox.addEventListener('mouseout', () => {
+	linkedin.setAttribute('src', 'images/icons/045-linkedin.png');
+	github.setAttribute('src', 'images/icons/038-github.png');
+	instagram.setAttribute('src', 'images/icons/025-instagram.png');
+	email.setAttribute(
+		'src',
 		'images/icons/black-back-closed-envelope-shape.png'
-	) {
-		email.setAttribute('src', 'images/icons/003-gmail.png');
-	}
-	// } else {
-	// 	email.setAttribute(
-	// 		'src',
-	// 		'images/icons/black-back-closed-envelope-shape.png'
-	// 	);
-	// }
+	);
+	resume.setAttribute('src', 'images/icons/005-profile-1.png');
 });
 
-resume.addEventListener('mouseover', () => {
-	if (resume.getAttribute('src') == 'images/icons/005-profile-1.png') {
-		resume.setAttribute('src', 'images/icons/004-profile.png');
-	}
-	// } else {
-	// 	resume.setAttribute('src', 'images/icons/005-profile-1.png');
-	// }
-});
-
-const contactForm = document.getElementById('contact-form');
+//CONTACT FORM UNDER CONSTRUCTION
+// const contactForm = document.getElementById('contact-form');
 
 // const frmvalidator = new Validator(contactForm);
 
